eb612c15d44eb57a63da8e0aecd148dee585a63a
Stephen Fox <stephen.j.fox.jr@gmail.com>
2026-04-26 11:07:19 -0400

    dhcpd: Added compat fn for OpenBSD's getrtable(2)
    Commit 9d7a36478719f85f4d082aabc1772c51f9f26d8d switched how dhcpd
    handles filtering interfaces by their rtable (or in FreeBSD, FIB).
    That commit also introduced the usage of getrtable(2), which does
    not have an equivalent syscall or libc function in FreeBSD.
    
    Instead, we use sysctl(3) in its place to get the dhcpd process'
    FIB and expose that code as a function named getrtable.

--- usr.sbin/dhcpd/dhcpd_freebsd_compat.h
+++ usr.sbin/dhcpd/dhcpd_freebsd_compat.h
@@ -6,6 +6,14 @@
 #define __dead
 #endif
 
+#include <sys/types.h>  // Needed by: getrtable
+#include <sys/sysctl.h> // Needed by: getrtable
+#include <sys/user.h>   // Needed by: getrtable
+#include <stdio.h>      // Needed by: getrtable
+#include <stdlib.h>     // Needed by: getrtable
+#include <unistd.h>     // Needed by: getrtable
+#include <err.h>        // Needed by: getrtable
+
 // Required for the following symbols in FreeBSD:
 // - INADDR_BROADCAST
 // - struct sockaddr_in
@@ -21,3 +29,23 @@ static inline int pledge(const char *promises, const char *execpromises) {
 	return 0;
 }
 #endif
+
+// getrtable stands in for OpenBSD's getrtable(2). This version uses
+// sysctl(3) to get the current process' FIB (the equivalent to rtable).
+static inline int
+getrtable(void)
+{
+        struct kinfo_proc kp;
+        size_t len = sizeof(kp);
+        int mib[4];
+
+        mib[0] = CTL_KERN;
+        mib[1] = KERN_PROC;
+        mib[2] = KERN_PROC_PID;
+        mib[3] = getpid();
+
+        if (sysctl(mib, 4, &kp, &len, NULL, 0) == -1)
+                err(1, "get process fib");
+
+        return kp.ki_fibnum;
+}
